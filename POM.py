bd8aee6de51309e8b52f519f51d99d65670bc342
Luís Coelho <luisportelacoelho@gmail.com>
2020-02-29 05:34:46 +0000

    Update the project structure in order that can be added new without change the old tests

---
+++ POM.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+import locators
+
+
+class ProductSelection:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def select_female(self):
+        fashion_selection = self.browser.find_elements_by_class_name(locators.FashionBeauty.FASHION_SELECTION)
+        female = fashion_selection[0]
+        female.click()
+
+    def select_product(self):
+        products = self.browser.find_elements_by_class_name(locators.FashionBeauty.PRODUCT_SELECTION)
+        product = products[0]
+        product.click()
+
+
+class Basket:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def addToBasket(self):
+        self.browser.find_element_by_css_selector(locators.ShoppingBasket.ADD_BASKET_BUTTON).click()
+
+    def confirmItem(self):
+        self.browser.find_element_by_css_selector(locators.ShoppingBasket.GO_SHOPPING_BASKET_BUTTON).click()
+        self.browser.find_element_by_css_selector(locators.ShoppingBasket.CONTINUE_DELIVERY_BUTTON).click()
+
+
+class DeliveryInformation:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def fillEmail(self, email):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.EMAIL).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.EMAIL).send_keys(email)
+
+    def placeOrder(self):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.PLACE_ORDER_GUEST_BUTTON).click()
+
+    def salutation(self):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.SALUTATION_MR_RADIO_BUTTON).click()
+
+    def firstName(self, first_name):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.FIRST_NAME).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.FIRST_NAME).send_keys(first_name)
+
+    def lastName(self, last_name):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.LAST_NAME).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.LAST_NAME).send_keys(last_name)
+
+    def address(self, address_1, address_2, postal_code, town):
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_1).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_1).send_keys(address_1)
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_2).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_2).send_keys(address_2)
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.POSTAL_CODE).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.POSTAL_CODE).send_keys(postal_code)
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_TOWN).clear()
+        self.browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_TOWN).send_keys(town)
+
+    def continueDelivery(self):
+        self.browser.find_element_by_css_selector(locators.ShoppingBasket.CONTINUE_DELIVERY_BUTTON).click()
+
+
+class Payment:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def page(self):
+        self.browser.find_element_by_css_selector(locators.PaymentPage.PAYMENT_RADIO_BUTTON)
+
+    def paymentVisa(self):
+        self.browser.find_element_by_css_selector(locators.PaymentPage.CREDIT_CARD_RADIO_BUTTON).click()
+        self.browser.find_element_by_css_selector(locators.PaymentPage.VISA_RADIO_BUTTON).click()
+
+    def confirmation(self):
+        self.browser.find_element_by_css_selector(locators.PaymentPage.CONFIRMATION_BUTTON).click()
--- test.py
+++ test.py
@@ -1,79 +1,56 @@
+import unittest
 import time
 import locators
 import drivers
 from selenium import webdriver
 from user import User
+from POM import ProductSelection
+from POM import Basket
+from POM import DeliveryInformation
+from POM import Payment
 
-SITE_URL = 'https://shop.mercedes-benz.com/en-gb/collection/'
-TIME_OUT_SECONDS = 10
+class Test(unittest.TestCase):
 
-browser = webdriver.Chrome(drivers.Drivers.CHROME_DRIVER_PATH)
-browser.implicitly_wait(TIME_OUT_SECONDS)
-browser.maximize_window()
-browser.get(SITE_URL)
+    def setUp(self):
+        SITE_URL = 'https://shop.mercedes-benz.com/en-gb/collection/'
+        TIME_OUT_SECONDS = 10
+        self.browser = webdriver.Chrome(drivers.Drivers.CHROME_DRIVER_PATH)
+        self.browser.implicitly_wait(TIME_OUT_SECONDS)
+        self.browser.maximize_window()
+        self.browser.get(SITE_URL)
+        self.browser.find_element_by_class_name(locators.Cookie.AGREE_BUTTON).click()
+        self.browser.get(SITE_URL)
 
+    def test_buy_merchandising(self):
 
-def selectItem():
+        productSelection = ProductSelection(self.browser)
+        productSelection.select_female()
+        productSelection.select_product()
 
-    try:
-        browser.find_element_by_class_name(locators.Cookie.AGREE_BUTTON).click()
-        browser.get(SITE_URL)
-        fashion_selection = browser.find_elements_by_class_name(locators.FashionBeauty.FASHION_SELECTION)
-        woman_selection = fashion_selection[0]
-        woman_selection.click()
-        woman_products = browser.find_elements_by_class_name(locators.FashionBeauty.PRODUCT_SELECTION)
-        woman_product = woman_products[0]
-        woman_product.click()
-    except:
-        browser.save_screenshot('screenshots/errorSelectProduct.png')
+        basket = Basket(self.browser)
+        basket.addToBasket()
+        basket.confirmItem()
 
-
-def addBasket():
-    try:
-        browser.find_element_by_css_selector(locators.ShoppingBasket.ADD_BASKET_BUTTON).click()
-        browser.find_element_by_css_selector(locators.ShoppingBasket.GO_SHOPPING_BASKET_BUTTON).click()
-        browser.find_element_by_css_selector(locators.ShoppingBasket.CONTINUE_DELIVERY_BUTTON).click()
-    except:
-        browser.save_screenshot('screenshots/errorAddBasket.png')
-
-
-def fillDeliveryInformation():
-    try:
-        browser.find_element_by_css_selector(locators.AddressDelivery.EMAIL).send_keys(User.EMAIL)
-        browser.find_element_by_css_selector(locators.AddressDelivery.PLACE_ORDER_GUEST_BUTTON).click()
-        browser.find_element_by_css_selector(locators.AddressDelivery.SALUTATION_MR_RADIO_BUTTON).click()
-        browser.find_element_by_css_selector(locators.AddressDelivery.FIRST_NAME).send_keys(User.FIRST_NAME)
-        browser.find_element_by_css_selector(locators.AddressDelivery.LAST_NAME).send_keys(User.LAST_NAME)
-        browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_1).send_keys(User.ADDRESS_1)
-        browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_2).send_keys(User.ADDRESS_2)
-        browser.find_element_by_css_selector(locators.AddressDelivery.POSTAL_CODE).send_keys(User.POSTAL_CODE)
-        browser.find_element_by_css_selector(locators.AddressDelivery.ADDRESS_TOWN).send_keys(User.ADDRESS_TOWN)
+        deliveryInformation = DeliveryInformation(self.browser)
+        deliveryInformation.fillEmail(User.EMAIL)
+        deliveryInformation.placeOrder()
+        deliveryInformation.salutation()
+        deliveryInformation.firstName(User.FIRST_NAME)
+        deliveryInformation.lastName(User.LAST_NAME)
+        deliveryInformation.address(User.ADDRESS_1, User.ADDRESS_2,
+            User.POSTAL_CODE, User.ADDRESS_TOWN)
         time.sleep(5)
-        browser.find_element_by_css_selector(locators.ShoppingBasket.CONTINUE_DELIVERY_BUTTON).click()
-    except:
-        browser.save_screenshot('screenshots/errorFillDeliveryInformation.png')
+        deliveryInformation.continueDelivery()
+
+        payment = Payment(self.browser);
+        payment.page()
+        payment.paymentVisa()
+        payment.confirmation()
 
 
-def payment():
-    try:
-        browser.find_element_by_css_selector(locators.PaymentPage.PAYMENT_RADIO_BUTTON)
-        browser.find_element_by_css_selector(locators.PaymentPage.CREDIT_CARD_RADIO_BUTTON).click()
-        browser.find_element_by_css_selector(locators.PaymentPage.VISA_RADIO_BUTTON).click()
-        browser.find_element_by_css_selector(locators.PaymentPage.CONFIRMATION_BUTTON).click()
-    except:
-        browser.save_screenshot('screenshots/errorPayment.png')
+    def tearDown(self):
+        self.browser.close()
 
 
-def close():
-    browser.close()
-
-
-try:
-    selectItem()
-    addBasket()
-    fillDeliveryInformation()
-    time.sleep(2)
-    payment()
-    close()
-except:
-    print('check the errors folder screenshot')
+if __name__ == "__main__":
+    unittest.main()
